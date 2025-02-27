a9af8ee4c14c95faa4c07994bce1b0daf9627d29
MaheshGSLAB <mahesh.kumar-ext@arista.com>
2025-02-27 14:54:41 +0530

    used the object to append the data

--- python-avd/pyavd/_eos_designs/structured_config/network_services/router_internet_exit.py
+++ python-avd/pyavd/_eos_designs/structured_config/network_services/router_internet_exit.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Protocol
 
+from pyavd._eos_cli_config_gen.schema import EosCliConfigGen
 from pyavd._eos_designs.structured_config.structured_config_generator import structured_config_contributor
 
 if TYPE_CHECKING:
@@ -28,19 +29,20 @@ class RouterInternetExitMixin(Protocol):
         if not self._filtered_internet_exit_policies_and_connections:
             return
 
+        policies = EosCliConfigGen.RouterInternetExit.Policies()
+        exit_groups = EosCliConfigGen.RouterInternetExit.ExitGroups()
         for policy, connections in self._filtered_internet_exit_policies_and_connections:
-            policy_exit_groups = []
             # TODO: Today we use the order of the connection list to order the exit-groups inside the policy.
             #       This works for zscaler but later we may need to use some sorting intelligence as order matters.
             for connection in connections:
                 exit_group_name = connection["exit_group"]
-                self.structured_config.router_internet_exit.exit_groups.obtain(exit_group_name).local_connections.append_new(name=connection["name"])
+                exit_groups.obtain(exit_group_name).local_connections.append_unique(
+                    EosCliConfigGen.RouterInternetExit.ExitGroupsItem.LocalConnectionsItem(name=connection["name"])
+                )
                 # Recording the exit_group in the policy
-                if exit_group_name not in policy_exit_groups:
-                    policy_exit_groups.append(exit_group_name)
+                policies.obtain(policy.name).exit_groups.append_unique(EosCliConfigGen.RouterInternetExit.PoliciesItem.ExitGroupsItem(name=exit_group_name))
 
             if policy.fallback_to_system_default:
-                policy_exit_groups.append("system-default-exit-group")
+                policies.obtain(policy.name).exit_groups.append_new(name="system-default-exit-group")
 
-            for exit_group_name in policy_exit_groups:
-                self.structured_config.router_internet_exit.policies.obtain(policy.name).exit_groups.append_new(name=exit_group_name)
+        self.structured_config.router_internet_exit._update(policies=policies, exit_groups=exit_groups)

50bf30219635c0ce67a4296bf0c6f70f41ac3d23
Claus Holbech <holbech@arista.com>
2025-03-26 10:35:34 +0100

    fix

--- python-avd/tests/pyavd/cv/client/test_cv_client.py
+++ python-avd/tests/pyavd/cv/client/test_cv_client.py
@@ -8,18 +8,23 @@ from os import environ
 from typing import TYPE_CHECKING
 from unittest.mock import patch
 
+import aristaproto
 import pytest
 import pytest_asyncio
 
-from tests.pyavd.cv.mockery import MockedServiceStub, RecordingServiceStub, mocked_cv_client_aenter
+from pyavd._cv.client import CVClient
+from tests.pyavd.cv.mockery import mocked_cv_client_aenter, playback_unary_stream, playback_unary_unary, recording_unary_stream, recording_unary_unary
 
 if TYPE_CHECKING:
     from collections.abc import AsyncGenerator
 
-    # Avoid importing this outside the mock.
-    from pyavd._cv.client import CVClient
-
 LOGGER = getLogger(__name__)
+
+# Environment variables
+# TODO: avoid having a default server and instead run tests to all recorded servers in offline mode.
+CV_SERVER = environ.get("CV_SERVER") or "www.cv-prod-us-central1-c.arista.io"
+CV_TOKEN = environ.get("CV_ACCESS_TOKEN")
+RECORDING = environ.get("RECORDING")
 
 
 @pytest_asyncio.fixture
@@ -35,32 +40,36 @@ async def cv_client() -> AsyncGenerator[CVClient, None]:
 
     Otherwise this will return an instance of CVClient where API calls are mocked using previously recorded API messages.
     """
-    cv_server = environ.get("CV_SERVER") or "www.cv-prod-us-central1-c.arista.io"
-
-    if token := environ.get("CV_ACCESS_TOKEN"):
-        LOGGER.info("Running in online mode connecting to %s.", cv_server)
-        if environ.get("RECORDING"):
+    if CV_SERVER and CV_TOKEN:
+        LOGGER.info("Running in online mode connecting to %s.", CV_SERVER)
+        if RECORDING:
             LOGGER.info("Mocking ServiceStub to RecordingServiceStub")
-            with patch("aristaproto.ServiceStub", new=RecordingServiceStub):
-                from pyavd._cv.client import CVClient
+            aristaproto.grpc.grpclib_client.ServiceStub._org_unary_unary = aristaproto.grpc.grpclib_client.ServiceStub._unary_unary
+            aristaproto.grpc.grpclib_client.ServiceStub._org_unary_stream = aristaproto.grpc.grpclib_client.ServiceStub._unary_stream
+            aristaproto.grpc.grpclib_client.ServiceStub._unary_unary = recording_unary_unary
+            aristaproto.grpc.grpclib_client.ServiceStub._unary_stream = recording_unary_stream
+            async with CVClient(servers=CV_SERVER, token=CV_TOKEN) as cv_client:
+                yield cv_client
 
-                async with CVClient(servers=cv_server, token=token) as cv_client:
-                    yield cv_client
+            aristaproto.grpc.grpclib_client.ServiceStub._unary_unary = aristaproto.grpc.grpclib_client.ServiceStub._org_unary_stream
+            aristaproto.grpc.grpclib_client.ServiceStub._unary_stream = aristaproto.grpc.grpclib_client.ServiceStub._org_unary_stream
 
         else:
-            from pyavd._cv.client import CVClient
-
-            async with CVClient(servers=cv_server, token=token) as cv_client:
+            async with CVClient(servers=CV_SERVER, token=CV_TOKEN) as cv_client:
                 yield cv_client
 
     else:
-        with patch("aristaproto.ServiceStub", new=MockedServiceStub):
-            from pyavd._cv.client import CVClient
+        LOGGER.info("Mocking ServiceStub to MockedServiceStub")
+        aristaproto.grpc.grpclib_client.ServiceStub._org_unary_unary = aristaproto.grpc.grpclib_client.ServiceStub._unary_unary
+        aristaproto.grpc.grpclib_client.ServiceStub._org_unary_stream = aristaproto.grpc.grpclib_client.ServiceStub._unary_stream
+        aristaproto.grpc.grpclib_client.ServiceStub._unary_unary = playback_unary_unary
+        aristaproto.grpc.grpclib_client.ServiceStub._unary_stream = playback_unary_stream
+        with patch("pyavd._cv.client.CVClient.__aenter__", new=mocked_cv_client_aenter):
+            async with CVClient(servers=CV_SERVER, token=CV_TOKEN) as cv_client:
+                yield cv_client
 
-            with patch("pyavd._cv.client.CVClient.__aenter__", new=mocked_cv_client_aenter):
-                async with CVClient(servers=cv_server, token=token) as cv_client:
-                    yield cv_client
-
+        aristaproto.grpc.grpclib_client.ServiceStub._unary_unary = aristaproto.grpc.grpclib_client.ServiceStub._org_unary_unary
+        aristaproto.grpc.grpclib_client.ServiceStub._unary_stream = aristaproto.grpc.grpclib_client.ServiceStub._org_unary_stream
         return
 
 
--- python-avd/tests/pyavd/cv/mockery.py
+++ python-avd/tests/pyavd/cv/mockery.py
@@ -8,19 +8,17 @@ from hashlib import sha1
 from logging import getLogger
 from os import environ
 from pathlib import Path
-from typing import TYPE_CHECKING
-
-from aristaproto.grpc.grpclib_client import MetadataLike, ServiceStub
+from typing import TYPE_CHECKING, NoReturn
 
 if TYPE_CHECKING:
     from collections.abc import AsyncIterator
     from typing import Any, TypeVar
 
     from aristaproto import Message
+    from aristaproto.grpc.grpclib_client import MetadataLike, ServiceStub
     from grpclib._typing import IProtoMessage
     from grpclib.metadata import Deadline
 
-    # Avoid importing this outside the mock.
     from pyavd._cv.client import CVClient
 
     T_Message = TypeVar("T_Message", bound=Message)
@@ -37,88 +35,93 @@ def get_recording_file(route: str, request: IProtoMessage, cv_server: str) -> Pa
     return recording_file
 
 
-class RecordingServiceStub(ServiceStub):
-    """Subclass of ServiceStub which records all API calls to JSON files."""
-
-    async def _unary_unary(
-        self,
-        route: str,
-        request: IProtoMessage,
-        response_type: type[T_Message],
-        *,
-        timeout: float | None = None,
-        deadline: Deadline | None = None,
-        metadata: MetadataLike | None = None,
-    ) -> T_Message:
-        LOGGER.info("Recording API request: %s", request)
-        recording_file = get_recording_file(route, request, cv_server=self.channel._host)
-        result = await super()._unary_unary(route, request, response_type, timeout=timeout, deadline=deadline, metadata=metadata)
-        recording_file.write_text(result.to_json(indent=4))
-        return result
-
-    async def _unary_stream(
-        self,
-        route: str,
-        request: IProtoMessage,
-        response_type: type[T_Message],
-        *,
-        timeout: float | None = None,
-        deadline: Deadline | None = None,
-        metadata: MetadataLike | None = None,
-    ) -> AsyncIterator[T_Message]:
-        LOGGER.info("Recording API request: %s", request)
-        recording_file = get_recording_file(route, request, cv_server=self.channel._host)
-        messages_as_json = []
-        async for message in super()._unary_stream(route, request, response_type, timeout=timeout, deadline=deadline, metadata=metadata):
-            messages_as_json.append(message.to_json(indent=4))
-            yield message
-
-        result = f"[{', '.join(messages_as_json)}]"
-        recording_file.write_text(result)
+async def recording_unary_unary(
+    self: ServiceStub,
+    route: str,
+    request: IProtoMessage,
+    response_type: type[T_Message],
+    *,
+    timeout: float | None = None,
+    deadline: Deadline | None = None,
+    metadata: MetadataLike | None = None,
+) -> T_Message:
+    LOGGER.info("Recording API request: %s", request)
+    recording_file = get_recording_file(route, request, cv_server=self.channel._host)
+    result = await self._org_unary_unary(route, request, response_type, timeout=timeout, deadline=deadline, metadata=metadata)
+    recording_file.write_text(result.to_json(indent=4))
+    return result
 
 
-class MockedServiceStub(ServiceStub):
-    """Subclass of ServiceStub which mocks all API calls responding from JSON files."""
+async def recording_unary_stream(
+    self: ServiceStub,
+    route: str,
+    request: IProtoMessage,
+    response_type: type[T_Message],
+    *,
+    timeout: float | None = None,
+    deadline: Deadline | None = None,
+    metadata: MetadataLike | None = None,
+) -> AsyncIterator[T_Message]:
+    LOGGER.info("Recording API request: %s", request)
+    recording_file = get_recording_file(route, request, cv_server=self.channel._host)
+    messages_as_json = []
+    async for message in self._org_unary_stream(route, request, response_type, timeout=timeout, deadline=deadline, metadata=metadata):
+        messages_as_json.append(message.to_json(indent=4))
+        yield message
 
-    async def _unary_unary(
-        self,
-        route: str,
-        request: IProtoMessage,
-        response_type: type[T_Message],
-        **_kwargs: Any,
-    ) -> T_Message:
-        LOGGER.info("Playing back recording for API request: %s", request)
-        recording_file = get_recording_file(route, request, cv_server=self.channel._host)
-        if not recording_file.exists():
-            raise FileNotFoundError(recording_file, "for request", request)
-        recording = recording_file.read_text()
-        return response_type().from_json(recording)
+    result = f"[{', '.join(messages_as_json)}]"
+    recording_file.write_text(result)
 
-    async def _unary_stream(
-        self,
-        route: str,
-        request: IProtoMessage,
-        response_type: type[T_Message],
-        **_kwargs: Any,
-    ) -> AsyncIterator[T_Message]:
-        LOGGER.info("Playing back recording for API request: %s", request)
-        recording_file = get_recording_file(route, request, cv_server=self.channel._host)
-        if not recording_file.exists():
-            raise FileNotFoundError(recording_file, "for request", request)
-        recording = recording_file.read_text()
-        for message_as_dict in json.loads(recording):
-            yield response_type().from_pydict(message_as_dict)
+
+# class MockedServiceStub(ServiceStub):
+#     """Subclass of ServiceStub which mocks all API calls responding from JSON files."""
+
+
+async def playback_unary_unary(
+    self: ServiceStub,
+    route: str,
+    request: IProtoMessage,
+    response_type: type[T_Message],
+    **_kwargs: Any,
+) -> T_Message:
+    LOGGER.info("Playing back recording for API request: %s", request)
+    recording_file = get_recording_file(route, request, cv_server=self.channel._host)
+    if not recording_file.exists():
+        raise FileNotFoundError(recording_file, "for request", request)
+    recording = recording_file.read_text()
+    return response_type().from_json(recording)
+
+
+async def playback_unary_stream(
+    self: ServiceStub,
+    route: str,
+    request: IProtoMessage,
+    response_type: type[T_Message],
+    **_kwargs: Any,
+) -> AsyncIterator[T_Message]:
+    LOGGER.info("Playing back recording for API request: %s", request)
+    recording_file = get_recording_file(route, request, cv_server=self.channel._host)
+    if not recording_file.exists():
+        raise FileNotFoundError(recording_file, "for request", request)
+    recording = recording_file.read_text()
+    for message_as_dict in json.loads(recording):
+        yield response_type().from_pydict(message_as_dict)
 
 
 async def mocked_cv_client_aenter(self: CVClient) -> CVClient:
     class MockedChannel:
-        def __init__(self, host: str | None = None) -> None:
-            self._host = host
-
         def close(self) -> None:
             pass
 
-    self._channel = MockedChannel(host=self._servers[0])
+        def request(self, *_args: list[Any], **_kwargs: dict[str, Any]) -> NoReturn:
+            msg = (
+                "The MockedChannel instance was called from the regular ServiceStub which should never happen. "
+                "Something went wrong with patching the aristaproto.ServiceStub."
+            )
+            raise NotImplementedError(msg)
+
+    self._channel = MockedChannel()
+    self._channel._host = self._servers[0]
     self._cv_version = None
     self._metadata = {}
     return self
